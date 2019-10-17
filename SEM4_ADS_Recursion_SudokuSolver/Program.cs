6d54839dcd06e71fda370fbd51223ff0e0f87e11
Andrei Mungiu <andrei_mungiu@yahoo.com>
2019-10-17 15:10:45 +0200

    Added solutions for NQueens and SudokuSolver

---
+++ SEM4_ADS_Recursion_SudokuSolver/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEM4_ADS_Recursion_SudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
